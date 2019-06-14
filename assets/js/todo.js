08fdd54cf88e1c93ea9998478c729e48272f55f7
Mikhail <mikhailmcinally12@gmail.com>
2019-06-14 12:38:40 -0700

    add remove todos functionality

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,3 +1,12 @@
+//check Off todos by clicking
 $("ul").on("click", "li", function () {
     $(this).toggleClass("completed");
+});
+
+//Click on X to delete todos
+$("span").click(function (event) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    event.stopPropagation();
 });

95a6dc8b1f84456537302dd0a0627cdc23dd72fc
Mikhail <mikhailmcinally12@gmail.com>
2019-06-14 13:20:13 -0700

    add new todo functionality and delete todo icon

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -4,9 +4,19 @@ $("ul").on("click", "li", function () {
 });
 
 //Click on X to delete todos
-$("span").click(function (event) {
+$("ul").on("click", "span", function (event) {
     $(this).parent().fadeOut(500, function () {
         $(this).remove();
     });
     event.stopPropagation();
+});
+
+//type a todo title than press enter to add new todo
+$("input[type='text'").keypress(function (event) {
+    if (event.which === 13) {
+        var todoTitle = $(this).val();
+        $(this).val("");
+
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoTitle + "</li>");
+    }
 });
